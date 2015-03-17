1bdd4a4c4faa64af07f7d49eaf4bca7891270315
= <=>
2015-03-17 07:02:43 +0000

    add big5

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,51 +2,34 @@ class ContentsController < ApplicationController
   require 'nokogiri'
   require 'open-uri'
   require 'rest_client'
-  #require 'tradsim'
 
   def index
   end
 
   def parse_url
-
     url = params[:url]
     user_agent = "Atomz/1.0"
-    resource = RestClient.get(url,:referer => "" ,:user_agent => user_agent, :cookies => { _ts_id: "12345678"})
+    resource = RestClient.get(url,:referer => "" ,:user_agent => user_agent, :cookies => { _ts_id: "12345678"}).force_encoding("big5")
     File.open("#{Rails.root}/public/robots.txt", "w"){|file| file.write(resource)}
-    @doc = Nokogiri::HTML(resource)
-    @doc.xpath("//iframe").each do |doc|
+    doc = Nokogiri::HTML(resource)
+    #binding.pry
+    @title = doc.css('div.auction-data').css('h2').text
+    @price = doc.css('div.auction-data').css('li.initiation').text
+    iframe_url = ""
+    doc.xpath("//iframe").each do |doc|
       if doc.attributes["src"]
-        @good = doc.attributes["src"].value unless doc.attributes["src"].value.blank?
+        iframe_url = doc.attributes["src"].value unless doc.attributes["src"].value.blank?
       end
     end
-    p @good
-    resource = RestClient.get(@good,:referer => url ,:user_agent => user_agent, :cookies => { _ts_id: "12345678"})
-    File.open("#{Rails.root}/public/robots.txt", "w"){|file| file.write(resource)}
-    f = File.open("#{Rails.root}/public/robots.txt")
-    @doc = Nokogiri::HTML(resource)
+    resource = RestClient.get(iframe_url,:referer => url ,:user_agent => user_agent, :cookies => { _ts_id: "12345678"}).force_encoding("big5")
+    #File.open("#{Rails.root}/public/robots.txt", "w"){|file| file.write(resource)}
+    #f = File.open("#{Rails.root}/public/robots.txt")
+    doc = Nokogiri::HTML(resource)
     @array = []
-    p @doc.xpath("//p").count
-    @doc.xpath("//p").each do |doc|
-      aaa = doc.text.unpack("C*").pack("U*")
-      #p aaa.set_encoding("utf-8","big-5")
-      #p aaa.encode("UTF-8", "iso8859-1")
-      #bbb = aaa.encode("iso8859-1","utf-8")
-      #p bbb.encoding
-      #p bbb.force_encoding('unicode')
-      p doc.text.encoding
-      p doc.text.encode("big5", :invalid => :replace, :undef => :replace, :replace => "?").encoding
-      @array << doc.text.encode("big5", :invalid => :replace, :undef => :replace, :replace => "?")
-      #converted_text = Iconv.civ
-      #puts Tradsim::to_trad(aaa)
-      #aaa.encode("Unicode")
-      #ec1 = Encoding::Converter.new "UTF-8","Big-5",:invalid=>:replace,:undef=>:replace,:replace=>""
-      #ec2 = Encoding::Converter.new "Big-5","Unicode",:invalid=>:replace,:undef=>:replace,:replace=>""
-      #aaa = ec2.convert ec1.convert aaa
-      #
+    doc.xpath("//p").each do |doc|
+      #p doc.text
+      @array << doc.text.gsub(/<h\d>[^>]*>/,'').gsub(/<img[^>]*>/,'').gsub(/<\/?[^>]*>/, '').gsub(/\s/,'')
     end
-    #ec1 = Encoding::Converter.new "UTF-8","Windows-1251",:invalid=>:replace,:undef=>:replace,:replace=>""
-    #ec2 = Encoding::Converter.new "Windows-1251","UTF-8",:invalid=>:replace,:undef=>:replace,:replace=>""
-    #doc = ec2.convert ec1.convert doc
     render :layout => false
   end
 
