c7d351311badc58cbe2d770c9487875ee5ca0b31
Gtar69 <chihweichang69@gmail.com>
2015-03-17 09:07:05 +0800

    big55

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,14 +8,28 @@ class ContentsController < ApplicationController
 
   def parse_url
     url = params[:url]
-    @url = params[:url]
     user_agent = "Atomz/1.0"
-    resource = RestClient.get @url, :user_agent => " user_agent"
+    resource = RestClient.get(url,:referer => "" ,:user_agent => user_agent, :cookies => { _ts_id: "12345678"})
     File.open("#{Rails.root}/public/robots.txt", "w"){|file| file.write(resource)}
     @doc = Nokogiri::HTML(resource)
-    p @doc.class
-    p @doc
-    #p @doc.xpath('//li').count
+    @doc.xpath("//iframe").each do |doc|
+      if doc.attributes["src"]
+        @good = doc.attributes["src"].value unless doc.attributes["src"].value.blank?
+      end   
+    end
+    p @good
+    resource = RestClient.get(@good,:referer => url ,:user_agent => user_agent, :cookies => { _ts_id: "12345678"})
+    File.open("#{Rails.root}/public/robots.txt", "w"){|file| file.write(resource)}
+    f = File.open("#{Rails.root}/public/robots.txt")
+
+    @doc = Nokogiri::HTML(resource, nil, 'UTF-8')
+    @array = []
+    p @doc.xpath("//p").count
+    @doc.xpath("//p").each do |doc| 
+    #  p doc.text.encode("UTF-8")
+      @array << doc.text
+    end
+
   end
 
 end
